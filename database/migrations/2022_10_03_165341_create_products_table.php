39b7c076cba771690b4f40e101e0607783730693
nzau <abrahamnzau@gmail.com>
2022-10-04 13:46:49 +0300

    Set up DB Table Migration and Models

---
+++ database/migrations/2022_10_03_165341_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', static function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku')->nullable();
+            $table->string('name')->nullable();
+            $table->string('category')->nullable();
+            $table->json('price')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
