62e1e456ae4cf02d475b2da6fac60c66e9bab712
nzau <abrahamnzau@gmail.com>
2022-10-04 15:38:03 +0300

    Define specific fields to be returned

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -3,11 +3,13 @@
 namespace App\Repositories;
 
 use App\Interfaces\ProductRepositoryInterface;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
 
 class ProductRepository implements ProductRepositoryInterface
 {
-    public function getAllProducts($category="", $min_price="", $max_price="")
+    public function getAllProducts($category="", $min_price="", $max_price=""): Collection
     {
-        return
+        return Product::all('sku', 'name','category', 'price->original as price');
     }
 }
